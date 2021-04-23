da3507201fbc89e67378d1a326ea2a366c614454
AntonAdonin <adonin-2004@mail.ru>
2021-04-23 18:40:41 +0700

    Скрипт для рисования на холсте

---
+++ script.js
@@ -0,0 +1,24 @@
+let cnv = document.getElementById("canvas")
+let ctx = cnv.getContext("2d");
+let color = "black";
+let width = 30;
+
+let displayWidth = cnv.clientWidth;
+let displayHeight = cnv.clientHeight;
+cnv.width = displayWidth;
+cnv.height = displayHeight;
+
+
+cnv.onmousedown = () => {
+    setTimeout(() => {
+        cnv.onmousemove = (event) => {
+            ctx.fillStyle = color;
+            ctx.fillRect(event.offsetX-width/2, event.offsetY-width/2, width, width);
+        };
+    }, 1);
+    ctx.fillStyle = color;
+    ctx.fillRect(e.offsetX-width/2, e.offsetY-width/2, width, width);
+    cnv.onmouseup = () =>{
+        cnv.onmousemove = null;
+    }
+}
